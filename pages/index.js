c9c9b8040ffa31c1f008b4d88de5552f2b68bb0b
Fabian Hainz <fabian.hainz@gravima.de>
2023-05-20 22:34:20 +0200

    add base

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faInstagram} from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+      <div className="bg-windows-blue h-screen w-screen">
+        <div>
+          <h1>Hallo</h1>
+        </div>
+      </div>
 
-      <Footer />
+      <footer className="bg-windows-grey h-12 w-full absolute bottom-0 border-[#D9E4EA] border-t-4">
+        <h1>ALL I NEED</h1>
+        <FontAwesomeIcon icon={faInstagram} />
+
+      </footer>
     </div>
   )
 }
